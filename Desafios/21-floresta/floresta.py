94f10c535f39715a8533e83c413f8198f07a04a5
TiagoCSoares <107574117+TiagoCSoares@users.noreply.github.com>
2023-02-09 21:14:23 -0300

    Add files via upload

--- Desafios/21-floresta/floresta.py
+++ Desafios/21-floresta/floresta.py
@@ -12,22 +12,63 @@ def retangulo(X):
             break
     return lakaka
 
-def area(bluepen,X):
+def gera_matriz(bluepen,X):
     largura = X[1]
     altura = X[0]
     matriz = np.ones((altura, largura), dtype=int)
+    a = 0
     for i in range (len(bluepen)):
         j = 1
         testes = bluepen[i]
         if testes[0] == 0:
             break
-        for j in range (len(bluepen)):
-            matriz[bluepen[i][j]][bluepen[i][j+1]] = 0
-                    
+        for j in range (1,len(bluepen[i]),2):
+            if j > len(bluepen[i]):
+                break
+            if j < 2:
+                matriz[bluepen[i][j]][bluepen[i][j+1]] = 0 
+            else:
+                matriz[(bluepen[i][j] + bluepen[i][1])][(bluepen[i][j+1] + bluepen[i][2])] = 0   
+
     return matriz
 
+def maior_retangulo(matriz,X):
+    altura = X[0]
+    largura = X[1]
+    histograma = [0] * largura
+    max_area = 0
 
-
+    for i in range(altura):
+        for j in range(largura):
+            if i == 0 or j == 0 or matriz[i][j] == 0:
+                histograma[j] = 0
+            else:
+                histograma[j] += 1
+        
+        stack = []
+        histograma.append(0)
+        for j in range(len(histograma)):
+            while stack and histograma[stack[-1]] > histograma[j]:
+                h = histograma[stack.pop()]
+                w = j if not stack else j - stack[-1] - 1
+                max_area = max(max_area, h * w)
+            stack.append(j)
+        
+    for i in range(altura):
+        for j in range(largura):
+            if matriz[i][j] == 0:
+                k = i
+                while k < altura and matriz[k][j] == 0:
+                    k += 1
+                h = k - i
+                k = j
+                while k < largura and matriz[i][k] == 0:
+                    k += 1
+                w = k - j
+                max_area = max(max_area, h * w)
+    
+    return max_area
+    
 def main():
     n = input()
     n = int(n)
@@ -41,9 +82,9 @@ def main():
                 result = X[0]*X[1]
                 print(result)
             else:
-                print(X)
-                result = area(bluepen,X)
-                print(result)
+                matriz = gera_matriz(bluepen,X)
+                max_area = maior_retangulo(matriz,X)
+                print(max_area)
 
         except EOFError:
             break
